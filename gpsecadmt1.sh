f4ff5cd44a573456d9b6179c492efe79db2cc70c
SkyM1raj <141164867+Aur3ns@users.noreply.github.com>
2025-02-03 22:15:20 +0100

    Update gpsecadmt1.sh

--- gpsecadmt1.sh
+++ gpsecadmt1.sh
@@ -25,3 +25,15 @@ samba-tool gpo setoptions "$GPO_NAME" --enable
 samba-tool gpo acl "$GPO_GUID" --assign="$OU_PATH"
 
 echo "✅ GPO '$GPO_NAME' appliquée avec succès à $OU_PATH"
+
+echo " Ajout des groupes aux tiers en cours..."
+samba-tool group addmembers "Remote Desktop Users" "Group_ADMT1"
+samba-tool group addmembers "Server Operators" "Group_ADMT1"
+samba-tool group addmembers "DnsAdmins" "Group_ADMT1"
+samba-tool group addmembers "Group Policy Creator Owners" "Group_ADMT1"
+samba-tool group addmembers "Event Log Readers" "Group_ADMT1"
+samba-tool group addmembers "Network Configuration Operators" "Group_ADMT1"
+samba-tool group addmembers "Performance Monitor Users" "Group_ADMT1"
+samba-tool group addmembers "Performance Log Users" "Group_ADMT1"
+
+echo "Fin de la Configuration"
