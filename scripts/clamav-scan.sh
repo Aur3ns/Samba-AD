644aa22a8c7f9191e7761c9332816341649a1c16
Aur3ns <141164867+Aur3ns@users.noreply.github.com>
2025-04-05 00:50:13 +0200

    Update clamav-scan.sh

--- scripts/clamav-scan.sh
+++ scripts/clamav-scan.sh
@@ -10,9 +10,9 @@ clamdscan -r --multiscan --fdpass /tmp >> "$LOGFILE" 2>/dev/null
 # === Rechercher les fichiers infectés ===
 grep "FOUND" "$LOGFILE" | cut -d: -f1 | while read -r file; do
   if [ -f "$file" ]; then
-    echo "$file: REMOVED" | tee -a "$LOGFILE" >> "$FORWARD_LOG"
     rm -f "$file"
-  else
-    echo "$file: FOUND ERROR" | tee -a "$LOGFILE" >> "$FORWARD_LOG"
   fi
 done
+
+# === Forward log à Wazuh ===
+grep "FOUND" "$LOGFILE" >> "$FORWARD_LOG"
