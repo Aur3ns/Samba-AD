f7fd1dc20443c2b451bee0d05003e3d1e64d446a
Aur3ns <141164867+Aur3ns@users.noreply.github.com>
2025-04-04 14:05:53 +0200

    Update clamav-scan.sh

--- scripts/clamav-scan.sh
+++ scripts/clamav-scan.sh
@@ -2,26 +2,30 @@
 
 # === Paramètres ===
 LOGFILE="/var/log/clamav/clamdscan.log"
+FORWARDED_LOG="/var/log/clamav/clamd-forwarding.log"
 TIMESTAMP=$(date '+%Y-%m-%d %H:%M:%S')
 
-echo "[$TIMESTAMP] [*] Début du scan ClamAV..." >> "$LOGFILE"
+echo "[$TIMESTAMP] [★] Beginning of the ClamAV scan..." >> "$LOGFILE"
 
 # === Scan complet avec clamdscan ===
-ionice -c3 -n7 nice -n19 clamdscan -r --multiscan --fdpass / >> "$LOGFILE" 2>&1
+ionice -c3 -n7 nice -n19 clamdscan -r --multiscan --fdpass /tmp >> "$LOGFILE" 2>&1
 
-echo "[$TIMESTAMP] [*] Scan terminé. Vérification des fichiers infectés..." >> "$LOGFILE"
+TIMESTAMP=$(date '+%Y-%m-%d %H:%M:%S')
+echo "[$TIMESTAMP] [✓] Scan Completed. Checking infected files..." >> "$LOGFILE"
 
-# === Rediriger les infections vers le log lu par Wazuh ===
-grep FOUND "$LOGFILE" >> /var/log/clamav/clamd-forwarded.log
+# === Rediriger les infections et suppressions vers le log lu par Wazuh ===
+grep -E 'FOUND|ClamAv: Removed' "$LOGFILE" >> "$FORWARDED_LOG"
 
 # === Suppression des fichiers infectés ===
-grep FOUND "$LOGFILE" | cut -d: -f1 | while read -r file; do
+grep -E '^[^:]+: .*FOUND$' "$LOGFILE" | cut -d: -f1 | while read -r file; do
+  TIMESTAMP=$(date '+%Y-%m-%d %H:%M:%S')
   if [ -f "$file" ]; then
-    echo "[$TIMESTAMP] [-] Suppression : $file" >> "$LOGFILE"
+    echo "[$TIMESTAMP] ClamAv: Removed : $file" | tee -a "$LOGFILE" >> "$FORWARDED_LOG"
     rm -f "$file"
   else
-    echo "[$TIMESTAMP] [?] Fichier introuvable : $file" >> "$LOGFILE"
+    echo "[$TIMESTAMP] [?] ClamAv Error: File not found : $file" >> "$LOGFILE"
   fi
 done
 
-echo "[$TIMESTAMP] [✓] Suppression terminée." >> "$LOGFILE"
+TIMESTAMP=$(date '+%Y-%m-%d %H:%M:%S')
+echo "[$TIMESTAMP] [✓] ClamAv Deletion completed" >> "$LOGFILE"
