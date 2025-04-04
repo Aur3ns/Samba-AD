8ea3d62321c29f2cc72d4742fbcc79009ad439b1
Aur3ns <141164867+Aur3ns@users.noreply.github.com>
2025-04-04 15:42:21 +0200

    Update clamav-scan.sh

--- scripts/clamav-scan.sh
+++ scripts/clamav-scan.sh
@@ -1,25 +1,23 @@
 #!/bin/bash
 
-# === Paramètres ===
+# === Fichiers de log ===
 LOGFILE="/var/log/clamav/clamdscan.log"
 FORWARD_LOG="/var/log/clamav/clamd-forwarding.log"
-TIMESTAMP=$(date '+%Y-%m-%d %H:%M:%S')
 
-# === Démarrer le scan ===
-echo "[$TIMESTAMP] WARNING: ClamAV scan started." | tee -a "$LOGFILE" "$FORWARD_LOG"
-ionice -c3 -n7 nice -n19 clamdscan -r --multiscan --fdpass /tmp >> "$LOGFILE" 2>&1
-echo "[$TIMESTAMP] WARNING: ClamAV scan completed." | tee -a "$LOGFILE" "$FORWARD_LOG"
+# === Scan silencieux ===
+clamdscan -r --multiscan --fdpass /tmp >> "$LOGFILE" 2>&1
 
 # === Extraire les fichiers infectés ===
 grep FOUND "$LOGFILE" | while IFS= read -r line; do
+    echo "$line" >> "$FORWARD_LOG"
+
     file_path=$(echo "$line" | cut -d: -f1)
     virus_name=$(echo "$line" | cut -d: -f2 | awk '{print $2}')
-    echo "[$TIMESTAMP] $file_path: $virus_name FOUND" | tee -a "$LOGFILE" "$FORWARD_LOG"
 
     if [ -f "$file_path" ]; then
         rm -f "$file_path"
-        echo "[$TIMESTAMP] $file_path: $virus_name REMOVED" | tee -a "$LOGFILE" "$FORWARD_LOG"
+        echo "$file_path: $virus_name REMOVED" >> "$FORWARD_LOG"
     else
-        echo "[$TIMESTAMP] $file_path: $virus_name ERROR" | tee -a "$LOGFILE" "$FORWARD_LOG"
+        echo "$file_path: $virus_name ERROR" >> "$FORWARD_LOG"
     fi
 done
